81d001306a4d883efb6fb1ee9cf91bdc64050e7c
Kishanraaj Kathiraso <99589890+kishankathimsu@users.noreply.github.com>
2024-09-21 17:23:51 +0800

    ""

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,7 +8,6 @@ function App() {
   const [Description, setDescription] = useState(null);
 
 
-
   async function GetListOfIssues() {
     try {
       const response = await axios.get("http://localhost:3001/issues");
